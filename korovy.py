7f25eddaccdacf938d0ec00523066b81585f93b5
l-yer-l-prog-l <62470172+l-yer-l-prog-l@users.noreply.github.com>
2020-03-29 15:26:10 +0600

    Delete korovy.py

--- korovy.py
+++
@@ -1,9 +0,0 @@
-N = str(input())
-N.append(0)
-last = int(N[1])
-if ("0", "5", "6", "7", "8", "9" in last):
-    print(N, 'korov')
-elif last == 1 or int(N) == 1:
-    print(N, 'korova')
-else:
-    print(N, 'korovy')
